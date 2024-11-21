73ab2b8f41c43009bb105e6f0ebc5eb98bb99587
jek <mynameisjek@yandex.ru>
2024-11-21 14:05:18 +0300

    v3 server is work

--- SERVER_V1/SERVER_V1.ino
+++ SERVER_V1/SERVER_V1.ino
@@ -12,18 +12,18 @@
 #define MOTOR1_S 4
 #define MOTOR2_D 16
 #define MOTOR2_S 17
-#define MOTOR3_D 2
-#define MOTOR3_S 4
-#define MOTOR4_D 16
-#define MOTOR4_S 17
-#define MOTOR5_D 2
-#define MOTOR5_S 4
-#define MOTOR6_D 16
-#define MOTOR6_S 17
-#define MOTOR7_D 2
-#define MOTOR7_S 4
-#define MOTOR8_D 16
-#define MOTOR8_S 17
+#define MOTOR3_D 5
+#define MOTOR3_S 18
+#define MOTOR4_D 19
+#define MOTOR4_S 21
+#define MOTOR5_D 13
+#define MOTOR5_S 12
+#define MOTOR6_D 14
+#define MOTOR6_S 27
+#define MOTOR7_D 33
+#define MOTOR7_S 32
+#define MOTOR8_D 22
+#define MOTOR8_S 23
 FTPServer ftp(SPIFFS);
 WebServer srv(80);
 class RobotX_Motor {
@@ -72,6 +72,15 @@ void setup() {
   SPIFFS.begin();
   ftp.begin(F_NAME, F_PASW);
   srv.on("/", home_p);
+  srv.on("/create_task", set_task);
+  srv.on("/p1", place1);
+  srv.on("/p2", place2);
+  srv.on("/p3", place3);
+  srv.on("/p4", place4);
+  srv.on("/p5", place5);
+  srv.on("/p6", place6);
+  srv.on("/p7", place7);
+  srv.on("/p8", place8);
   srv.begin();
 }
 
@@ -89,8 +98,7 @@ void loop() {
     del_timer = millis();
   }
 }
-
-void home_p() {
+void set_task() {
   //pl=2&prt1=2&prt2=3&val1=1&val2=255&t=1000
   if (srv.hasArg("pl") and srv.hasArg("val1") and srv.hasArg("val2") and srv.hasArg("t")) {
     int place = (srv.arg("pl")).toInt();
@@ -102,82 +110,91 @@ void home_p() {
         motor_base[0].ndigitalWrite(val1);
         motor_base[0].nanalogWrite(val2);
         task_target[0] = millis() + t;
-        get_file_content_server_send("/module1.html");
+        //get_file_content_server_send("",1);
         break;
       case 2:
         motor_base[1].ndigitalWrite(val1);
         motor_base[1].nanalogWrite(val2);
         task_target[1] = millis() + t;
-        get_file_content_server_send("/module2.html");
+        //get_file_content_server_send("",1);
         break;
       case 3:
         motor_base[2].ndigitalWrite(val1);
         motor_base[2].nanalogWrite(val2);
         task_target[2] = millis() + t;
-        get_file_content_server_send("/module3.html");
+        //get_file_content_server_send("",1);
         break;
       case 4:
         motor_base[3].ndigitalWrite(val1);
         motor_base[3].nanalogWrite(val2);
         task_target[3] = millis() + t;
-        get_file_content_server_send("/module4.html");
+        //get_file_content_server_send("",1);
         break;
       case 5:
         motor_base[4].ndigitalWrite(val1);
         motor_base[4].nanalogWrite(val2);
         task_target[4] = millis() + t;
-        get_file_content_server_send("/module5.html");
+        //get_file_content_server_send("",1);
         break;
       case 6:
         motor_base[5].ndigitalWrite(val1);
         motor_base[5].nanalogWrite(val2);
         task_target[5] = millis() + t;
-        get_file_content_server_send("/module6.html");
+        // get_file_content_server_send("",1);
         break;
       case 7:
         motor_base[6].ndigitalWrite(val1);
         motor_base[6].nanalogWrite(val2);
         task_target[6] = millis() + t;
-        get_file_content_server_send("/module7.html");
+        //get_file_content_server_send("",1);
         break;
       case 8:
         motor_base[8].ndigitalWrite(val1);
         motor_base[8].nanalogWrite(val2);
         task_target[8] = millis() + t;
-        get_file_content_server_send("/module8.html");
+        // get_file_content_server_send("",1);
         break;
     }
-  } else if (srv.hasArg("ports")) {
-    String port = srv.arg("ports");
-    if (port == "p1") {
-      get_file_content_server_send("/module1.html");
-    } else if (port == "p2") {
-      get_file_content_server_send("/module2.html");
-    } else if (port == "p3") {
-      get_file_content_server_send("/module3.html");
-    } else if (port == "p4") {
-      get_file_content_server_send("/module4.html");
-    } else if (port == "p5") {
-      get_file_content_server_send("/module5.html");
-    } else if (port == "p6") {
-      get_file_content_server_send("/module6.html");
-    } else if (port == "p7") {
-      get_file_content_server_send("/module7.html");
-    } else if (port == "p8") {
-      get_file_content_server_send("/module8.html");
-    }
-
-  } else {
-    get_file_content_server_send("/index.html");
   }
 }
-void get_file_content_server_send(String f_name) {
-  if (SPIFFS.exists(f_name)) {
-    File f_site = SPIFFS.open(f_name, "r");
-    String page = f_site.readString();
-    f_site.close();
-    srv.send(200, "text/html", page);
+void home_p() {
+    get_file_content_server_send("/index.html", 0);
+}
+void place1(){
+  get_file_content_server_send("/module1.html", 0);
+}
+void place2(){
+  get_file_content_server_send("/module2.html", 0);
+}
+void place3(){
+  get_file_content_server_send("/module3.html", 0);
+}
+void place4(){
+  get_file_content_server_send("/module4.html", 0);
+}
+void place5(){
+  get_file_content_server_send("/module5.html", 0);
+}
+void place6(){
+  get_file_content_server_send("/module6.html", 0);
+}
+void place7(){
+  get_file_content_server_send("/module7.html", 0);
+}
+void place8(){
+  get_file_content_server_send("/module8.html", 0);
+}
+void get_file_content_server_send(String f_name, int mode) {
+  if (mode == 0) {
+    if (SPIFFS.exists(f_name)) {
+      File f_site = SPIFFS.open(f_name, "r");
+      String page = f_site.readString();
+      f_site.close();
+      srv.send(200, "text/html", page);
+    } else {
+      srv.send(404, "text/plain", "File not found");
+    }
   } else {
-    srv.send(404, "text/plain", "File not found");
+    srv.send(200, "text/plain", "1");
   }
 }
